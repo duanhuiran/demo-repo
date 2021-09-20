165c72444111d3ef5e67a25fa58bf1bd89444bee
DuanHuiran <570989207@qq.com>
2021-09-21 01:55:30 +0200

    Commit Messages on my own! let's see where it will show?

--- test_python.py
+++ test_python.py
@@ -1,3 +1,5 @@
 # let's see how can I change the code on PyCharm!
 
 # Yes I can change the code on Pycharm!
+
+# If I klick the push derectly? Nothing happened if I didn't klick the COMMIT at first.
